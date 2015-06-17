05f915366a30a018060a7ca12427b1d96e986d1a
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2015-06-17 18:54:56 +0000

    Added dummy test
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/FeatureModel/Core/trunk@27246 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.sdq.featuremodel.tests/src/de/uka/ipd/sdq/featuremodel/tests/DummyTest.java
@@ -0,0 +1,17 @@
+package de.uka.ipd.sdq.featuremodel.tests;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Before
+    public void initialise() {
+    }
+
+    @Test
+    public void dummyTest() throws IOException, InterruptedException {
+    }
+}
